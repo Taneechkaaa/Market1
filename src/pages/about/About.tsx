fb4f49b13c458c15a3333d6c9eb5795ca91ead82
Tanya Bondarenko <taneechkaaa@mail.ru>
2024-09-27 16:34:54 +0700

    Сделала страницу About

--- src/pages/about/About.tsx
+++ src/pages/about/About.tsx
@@ -1,3 +1,23 @@
+import "./about.css";
+
 export const About = () => {
-  return <h1>about</h1>;
+  return (
+    <div className="about">
+      <img src="./team/team-pic.png" alt="" />
+      <div className="about-teamContent">
+        <h1 className="teamContent-header">Наша команда</h1>
+        <p className="teamContent-text">
+          Значимость этих проблем настолько очевидна, что базовый вектор
+          развития позволяет оценить значение экспериментов, поражающих по своей
+          масштабности и грандиозности. Мы вынуждены отталкиваться от того, что
+          консультация с широким активом.
+        </p>
+        <div className="team-avatars">
+          <img src="./team/team-1.png" alt="" />
+          <img src="./team/team-2.png" alt="" />
+          <img src="./team/team-3.png" alt="" />
+        </div>
+      </div>
+    </div>
+  );
 };
